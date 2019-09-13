b711d3ffe0b63273a4ba841de375494029bebbba
Yashwant <yashwanttyagi494@gmail.com>
2019-09-13 17:14:32 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,197 @@
+[
+  {
+    "GroupKey": "February 2017",
+    "TotalSpent": 484.64,
+    "TotalHsaSpent": 0,
+    "ProductList": [
+      "Banana",
+      "Tylinol",
+      "Flonaise",
+      "Water Bottle"
+    ],
+    "ReceiptIdList": [
+      2,
+      15,
+      46
+    ]
+  },
+  {
+    "GroupKey": "January 2017",
+    "TotalSpent": 110.44,
+    "TotalHsaSpent": 0,
+    "ProductList": [
+      "Flonaise",
+      "DayQuil"
+    ],
+    "ReceiptIdList": [
+      5,
+      12,
+      36,
+      37,
+      64,
+      73,
+      87
+    ]
+  },
+  {
+    "GroupKey": "October 2016",
+    "TotalSpent": 703.31,
+    "TotalHsaSpent": 0,
+    "ProductList": [
+      "Tylinol",
+      "Water Bottle",
+      "Flonaise",
+      "DayQuil"
+    ],
+    "ReceiptIdList": [
+      9,
+      11,
+      14,
+      27,
+      74,
+      78,
+      93
+    ]
+  },
+  {
+    "GroupKey": "November 2016",
+    "TotalSpent": 1566.88,
+    "TotalHsaSpent": 0,
+    "ProductList": [
+      "Banana",
+      "DayQuil",
+      "Flonaise",
+      "Tylinol",
+      "Water Bottle"
+    ],
+    "ReceiptIdList": [
+      13,
+      26,
+      40,
+      66,
+      88,
+      89,
+      90
+    ]
+  },
+  {
+    "GroupKey": "December 2016",
+    "TotalSpent": 920.9,
+    "TotalHsaSpent": 0,
+    "ProductList": [
+      "Tylinol",
+      "Banana",
+      "DayQuil",
+      "Water Bottle"
+    ],
+    "ReceiptIdList": [
+      28,
+      43,
+      48,
+      50,
+      57,
+      59,
+      61,
+      62,
+      81,
+      91,
+      99
+    ]
+  },
+  {
+    "GroupKey": "March 2017",
+    "TotalSpent": 1642.72,
+    "TotalHsaSpent": 84.32,
+    "ProductList": [
+      "Tacos",
+      "Mtn Dew",
+      "Mac Mouse",
+      "Drone",
+      "Tylinol",
+      "Test Sabrent Item",
+      "test lac",
+      "Eraser",
+      "WS-853",
+      "Sabrent",
+      "Mac and Cheese",
+      "Belladonna",
+      "Metronidazole",
+      "ASPIRIN, CHLORPHENIRAMINE MALEATE, and PHENYLEPHRINE BITARTRATE",
+      "Lisinopril",
+      "Naproxen Sodium",
+      "ORG VEG TRAY",
+      "PCK SALAD",
+      "Gl.D PEAK TEA"
+    ],
+    "ReceiptIdList": [
+      101,
+      102,
+      103,
+      107,
+      108,
+      109,
+      110,
+      111,
+      112,
+      113,
+      114,
+      115,
+      116,
+      117,
+      118,
+      119,
+      120,
+      121,
+      122,
+      123,
+      124,
+      125,
+      126,
+      127,
+      128,
+      129,
+      130,
+      134,
+      135,
+      137,
+      138,
+      139,
+      140,
+      141
+    ]
+  },
+  {
+    "GroupKey": "April 2017",
+    "TotalSpent": 923.45,
+    "TotalHsaSpent": 0,
+    "ProductList": [
+      "R",
+      "TAX 2 5.9",
+      "1 Cheese Fries",
+      "1 Seagrams 7",
+      "R ORG VEG TRAY",
+      "R PCK SALAD",
+      "R GID PEAK TEA",
+      "13PM Tota",
+      "Tax",
+      "Subtota",
+      "X",
+      "",
+      "I OTAL",
+      ")",
+      "1 l ()IAI PUNRCHASt.",
+      "13PM Total"
+    ],
+    "ReceiptIdList": [
+      152,
+      153,
+      156,
+      157,
+      166,
+      167,
+      170,
+      172,
+      173
+    ]
+  }
+]
